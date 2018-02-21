c4fc185ef54e873a89f53ea3e9be8a2a11beacc0
Anoop Sundaresh <as11552@nyu.edu>
2018-02-20 22:05:59 -0500

    Updated Head to have commands from user input.

--- head.c
+++ head.c
@@ -7,17 +7,17 @@
 #include "stat.h"
 #include "user.h"
 
-char buf[512];
+char accumBuff[512];
 
 void head(int fd, int noOfLines)
 {
 	int n, lines;
 
 	lines = 0;
-	while(((n = read(fd, buf, sizeof(buf))) > 0) && lines < noOfLines) {
+	while(((n = read(fd, accumBuff, sizeof(accumBuff))) > 0) && lines < noOfLines) {
 		for(int i=0; i<n && lines < noOfLines; i++){
-			if (buf[i] != '\n')
-				printf(1, "%c", buf[i]);
+			if (accumBuff[i] != '\n')
+				printf(1, "%c", accumBuff[i]);
 			else
 			{
 				printf(1, "\n");
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 {
 	int fd, i, lineCount;
 
-	// If user has used redirection on the command line
+	// If user gives direct input on the command line
 	if(argc <= 1){
 		lineCount = 10;
 		head(0, lineCount);
@@ -52,11 +52,11 @@ int main(int argc, char *argv[])
 			}
 			case 3 :
 			{
-				char l_str[10];
-				strcpy(l_str, argv[1]);
-				char *l_cptr = l_str;
-				l_cptr = l_cptr + 1; //Compensating for the hyphen(-)
-				lineCount = atoi(l_cptr);
+				char lStr[10];
+				strcpy(lStr, argv[1]);
+				char *lCptr = lStr;
+				lCptr = lCptr + 1; //Compensating for the hyphen(-)
+				lineCount = atoi(lCptr);
 				i = 2;
 				break;
 			}
